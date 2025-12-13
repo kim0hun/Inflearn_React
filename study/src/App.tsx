cd6db127a85bb83cefcfe8f9e18b7dc8a7781df8
Hun <dudgns9194@gmail.com>
2025-12-13 23:00:59 +0900

    30. 기본 데이터 출력하기

--- study/src/App.tsx
+++ study/src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+export default function App() {
 
-function App() {
-  const [count, setCount] = useState(0)
+  const primitiveString = "Hello, World!";
+  const primitiveNumber = 42;
+  const primitiveBoolean = false;
+  const primitiveUndefined = undefined;
+  const primitiveNull = null;
+  const primitiveSymbol = Symbol("mySymbol");
+  const primitiveBigInt = 9007199254740991n;
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>기본 자료형 출력</h2>
+      <p>문자열: {primitiveString}</p>
+      <p>숫자: {primitiveNumber}</p>
+      <p>논리형: {primitiveBoolean.toString()}</p>
+      <p>undefined: {String(primitiveUndefined)}</p>
+      <p>null: {String(primitiveNull)}</p>
+      <p>symbol: {String(primitiveSymbol)}</p>
+      <p>BigInt: {primitiveBigInt}</p>
     </>
-  )
+  );
 }
-
-export default App
