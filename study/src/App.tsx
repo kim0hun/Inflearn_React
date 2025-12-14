f74c90a336033985cc02904f9671bdcd8636a8c8
Hun <dudgns9194@gmail.com>
2025-12-14 15:26:58 +0900

    51. 이벤트 전파 - 버블링

--- study/src/App.tsx
+++ study/src/App.tsx
@@ -1,18 +1,9 @@
-import Button from "./components/ui/Button";
+import Table from "./components/Table";
 
 export default function App() {
-  const handleClick = (
-    message: string,
-    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  ) => {
-    console.log(message);
-    console.log(event);
-    (event.target as HTMLButtonElement).innerText = message;
-  };
-
   return (
     <>
-      <Button handleClick={handleClick} />
+      <Table />
     </>
   );
 }
