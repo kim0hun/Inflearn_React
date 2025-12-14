fcf75a9ae0b3aea4c4731af4dc50f59975ee5b1c
Hun <dudgns9194@gmail.com>
2025-12-14 14:19:48 +0900

    45. 이벤트 핸들러 방법 추천

--- study/src/components/ui/Button.tsx
+++ study/src/components/ui/Button.tsx
@@ -1,10 +1,11 @@
 export default function Button() {
-  const handleClick = (value: string) => alert(value);
+  const handleClick = () => alert("click");
 
   return (
     <>
-      <button onClick={() => handleClick("Hello")}>클릭</button >
-      <button onClick={() => alert("World!")}>클릭</button >
+      {/* <button onClick={() => alert("click")}>클릭</button > */}
+      <button onClick={handleClick}>클릭</button > {/*매개변수 X*/}
+      <button onClick={() => handleClick()}>클릭</button > {/*매개변수 O*/}
     </>
   );
 }
