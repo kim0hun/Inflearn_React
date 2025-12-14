f19bc3fcf8460a674aa899ad6afb468f796e7554
Hun <dudgns9194@gmail.com>
2025-12-14 13:43:02 +0900

    36. 타입 쉽게 알아내기

--- study/src/components/PrintValue.tsx
+++ study/src/components/PrintValue.tsx
@@ -1,28 +1,12 @@
-export default function PrintValue(props: {
-  numberValue: number;
-  stringValue: string;
-  booleanValue: boolean;
-  arrayValue: number[];
-  objectValue: { name: string; age: number };
-  handleClick: () => void;
-}) {
-  const {
-    numberValue,
-    stringValue,
-    booleanValue,
-    arrayValue,
-    objectValue,
-    handleClick
-  } = props;
-
+export default function PrintValue(props: { numberValue: number; stringValue: string; booleanValue: boolean; arrayValue: number[]; objectValue: { name: string; age: number; }; handleClick: () => void; }) {
   return (
     <>
-      <p>number: {numberValue}</p>
-      <p>string: {stringValue}</p>
-      <p>boolean: {booleanValue.toString()}</p>
-      <p>array: {arrayValue}</p>
-      <p>object: {JSON.stringify(objectValue)}</p>
-      <p>function: {handleClick.toString()}</p>
+      <p>number: {props.numberValue}</p>
+      <p>string: {props.stringValue}</p>
+      <p>boolean: {props.booleanValue.toString()}</p>
+      <p>array: {props.arrayValue}</p>
+      <p>object: {JSON.stringify(props.objectValue)}</p>
+      <p>function: {props.handleClick.toString()}</p>
     </>
   );
 }
