a41937becad265ae3ab8b734f5c7dcf8530ddce0
Hun <dudgns9194@gmail.com>
2025-12-14 15:35:32 +0900

    53. 이벤트 기본 동작 막기

--- study/src/App.tsx
+++ study/src/App.tsx
@@ -1,9 +1,19 @@
-import Table from "./components/Table";
-
 export default function App() {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+
   return (
     <>
-      <Table />
+      <form onSubmit={handleSubmit}>
+        <a
+          href="https://ko.wikipedia.org/"
+          onClick={(event) => event.preventDefault()}
+        >
+          위키피디아
+        </a>
+        <button type="submit">전송</button>
+      </form>
     </>
   );
 }
