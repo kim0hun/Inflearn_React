e2470434b7c57543df8e6a8a7a17a890394a801d
kim0hun <dudgns9194@gmail.com>
2025-12-20 23:51:01 +0900

    56. useState - 1

--- study/src/App.tsx
+++ study/src/App.tsx
@@ -1,16 +1,17 @@
+import { useState } from "react";
+
 export default function App() {
-  let name = "mike";
-  let age = 20;
-  const handleClick = () => {
-    name = "john";
-    age = 30;
-    console.log(name, age);
+  const [state, setState] = useState<number>(0);
+
+  const handleSetToTen = () => {
+    let a = state + 1;
+    setState(a);
   };
+
   return (
     <>
-      <p>name: {name}</p>
-      <p>age: {age}</p>
-      <button onClick={handleClick}>change</button>
+      <h1>state: {state}</h1>
+      <button onClick={handleSetToTen}>Set To 10</button>
     </>
   );
 }
