ff58b039bc9c15ffd64955dbb9f772e835a7b445
kim0hun <dudgns9194@gmail.com>
2025-12-21 00:09:25 +0900

    58. (미션) 상태값 정의하기

--- study/src/App.tsx
+++ study/src/App.tsx
@@ -1,19 +1,22 @@
 import { useState } from "react";
 
 export default function App() {
-  const [numberToString, setNumberToString] = useState<number | string>(0);
-  const [isShow, setIsShow] = useState<boolean>(true);
+  const [name, setName] = useState<string>("jack");
+  const [age, setAge] = useState<number>(20);
+  const [gender, setGender] = useState<"male" | "female">("male");
 
-  const handleSetToTen = () => {
-    setNumberToString("10");
-    setIsShow(false);
+  const handleUpdateProfile = () => {
+    setName("react");
+    setAge(21);
+    setGender("female");
   };
 
   return (
     <>
-      <h1>state: {numberToString}</h1>
-      <h1>isShow: {isShow.toString()}</h1>
-      <button onClick={handleSetToTen}>Set To 10</button>
+      <p>name: {name}</p>
+      <p>age: {age}</p>
+      <p>gender: {gender}</p>
+      <button onClick={handleUpdateProfile}>Update Profile</button>
     </>
   );
 }
