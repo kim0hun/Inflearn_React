6437125839d8bdfd056e2b79255b758946e80d9a
CSEHOON <younghoon9194@gmail.com>
2025-12-29 15:03:32 +0900

    72. useReducer

--- study/src/App.tsx
+++ study/src/App.tsx
@@ -1,9 +1,15 @@
-import Count from "./components/Count";
+import { useReducer } from "react";
+import counterReducer from "./reducer/counterReducer";
 
 export default function App() {
+  const [count, countDispatch] = useReducer(counterReducer, 0);
+
   return (
     <>
-      <Count />
+      <h1>Count: {count}</h1>
+      <button onClick={() => countDispatch({ type: "DECREMENT" })}>감소</button>
+      <button onClick={() => countDispatch({ type: "RESET" })}>리셋</button>
+      <button onClick={() => countDispatch({ type: "INCREMENT" })}>증가</button>
     </>
   );
 }
