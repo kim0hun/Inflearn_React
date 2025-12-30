874e67ff69a49ad75e4535e972d355b473888e81
CSEHOON <younghoon9194@gmail.com>
2025-12-30 14:53:15 +0900

    80. 조건부 렌더링 - && 연산자

--- study/src/App.tsx
+++ study/src/App.tsx
@@ -1,14 +1,10 @@
-import { useState } from "react";
-
 export default function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  const isLoggedIn = false;
 
   return (
     <>
-      <h1>{isLoggedIn ? "환영합니다" : "로그인이 필요합니다."}</h1>
-      <button onClick={() => setIsLoggedIn((isLoggedIn) => !isLoggedIn)}>
-        {isLoggedIn ? "로그아웃" : "로그인"}
-      </button>
+      {isLoggedIn && <h1>Welcome Back!</h1>}
+      {!isLoggedIn && <h1>Please Sign Up!</h1>}
     </>
   );
 }
