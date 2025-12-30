a576f0e630e8e09aca9ba927e94162e1a4de050a
CSEHOON <younghoon9194@gmail.com>
2025-12-30 15:39:47 +0900

    83. 반복 렌더링 - map

--- study/src/App.tsx
+++ study/src/App.tsx
@@ -1,14 +1,14 @@
 export default function App() {
   const fruits = ["apple", "banana", "orange"];
-  const items = [];
-  for (let i = 0; i < fruits.length; i++) {
-    items.push(<li key={i}>{fruits[i]}</li>);
-  }
 
   return (
     <>
       <p>Fruits Lists</p>
-      <ul>{items}</ul>
+      <ul>
+        {fruits.map((value, index) => (
+          <li key={index}>{value}</li>
+        ))}
+      </ul>
     </>
   );
 }
