803528eb6a173a930f449ec07771e99190cd58be
CSEHOON <younghoon9194@gmail.com>
2025-12-30 13:49:56 +0900

    74. 조건부 렌더링 -if

--- study/src/App.tsx
+++ study/src/App.tsx
@@ -1,9 +1,19 @@
-import Count from "./components/Count";
+// import GuestView from "./components/GuestView";
+// import UserInfoView from "./components/UserInfoView";
+// import UserView from "./components/UserView";
+
+import Greeting from "./components/Greeting";
 
 export default function App() {
-  return (
-    <>
-      <Count />
-    </>
-  );
+  const isLoggedIn = true;
+
+  return <Greeting isLoggedIn={isLoggedIn} />;
+  // if (isLoggedIn)
+  //   return (
+  //     <>
+  //       <UserView />
+  //       <UserInfoView />
+  //     </>
+  //   );
+  // else return <GuestView />;
 }
