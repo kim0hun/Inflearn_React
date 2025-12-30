6b30d56f0a0e0432fb648a291c82793fbeb74ec5
CSEHOON <younghoon9194@gmail.com>
2025-12-30 14:12:13 +0900

    76. 조건부 렌더링 - switch

--- study/src/App.tsx
+++ study/src/App.tsx
@@ -1,16 +1,11 @@
-import { useState } from "react";
-import LoginStatus from "./components/LoginStatus";
+import StatusMessage from "./components/StatusMessage";
 
 export default function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
-  const handleLogout = () => setIsLoggedIn(false);
-  const handleLogin = () => setIsLoggedIn(true);
+  const status = "un";
 
   return (
-    <LoginStatus
-      isLoggedIn={isLoggedIn}
-      handleLogin={handleLogin}
-      handleLogout={handleLogout}
-    />
+    <>
+      <StatusMessage status={status} />
+    </>
   );
 }
