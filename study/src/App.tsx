6219d25fffe9836c0f3dd469df3fc87efdfb5c0c
CSEHOON <younghoon9194@gmail.com>
2025-12-31 09:39:29 +0900

    85. key 속성 고유 하기

--- study/src/App.tsx
+++ study/src/App.tsx
@@ -1,9 +1,29 @@
-import FruitList from "./components/FruitList";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 export default function App() {
+  const items = ["사과", "바나나", "오렌지"];
+  const [fruits, setFruits] = useState(() =>
+    items.map((item) => ({
+      id: uuidv4(),
+      value: item,
+    }))
+  );
+  const handleFruitAdd = () => {
+    setFruits([{ id: uuidv4(), value: "포도" }, ...fruits]);
+  };
+
   return (
     <>
-      <FruitList />
+      <h1>과일목록</h1>
+      <ul>
+        {fruits.map((fruit) => (
+          <li key={fruit.id}>
+            <input type="text" placeholder={fruit.value} />
+          </li>
+        ))}
+      </ul>
+      <button onClick={handleFruitAdd}>과일 추가</button>
     </>
   );
 }
