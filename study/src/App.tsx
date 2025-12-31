08d86b3a70100768a494c35ba9efc137416cbe51
CSEHOON <younghoon9194@gmail.com>
2025-12-31 10:24:53 +0900

    88. (미션) 레시피 리스트 관리 앱 만들기

--- study/src/App.tsx
+++ study/src/App.tsx
@@ -1,30 +1,9 @@
-import { useState } from "react";
+import RecipeList from "./components/RecipeList";
 
 export default function App() {
-  const [items, setItems] = useState([
-    { id: 1, name: "Apple", category: "Fruit" },
-    { id: 2, name: "Carrot", category: "Vegetable" },
-    { id: 3, name: "Banana", category: "Fruit" },
-    { id: 4, name: "Tomato", category: "Vegetable" },
-  ]);
-
-  const handleDelete = (id: number) => {
-    setItems((items) => items.filter((item) => item.id !== id));
-  };
-
   return (
     <>
-      <h3>item list</h3>
-      <ul>
-        {items
-          // .filter((item) => item.category === "Vegetable")
-          .map((item) => (
-            <li key={item.id}>
-              {item.category} - {item.name}
-              <button onClick={() => handleDelete(item.id)}>Delete</button>
-            </li>
-          ))}
-      </ul>
+      <RecipeList />
     </>
   );
 }
