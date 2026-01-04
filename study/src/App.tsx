46b62b236d6e7c6d38e1f9760594f62fc7463fa7
Hun <dudgns9194@gmail.com>
2026-01-04 17:43:05 +0900

    101. styled-component (팁)

--- study/src/App.tsx
+++ study/src/App.tsx
@@ -18,6 +18,9 @@ const Title = styled.h1<{ $color: string; $decoration: string }>`
   color: ${(props) => props.$color};
   text-decoration: ${(props) => props.$decoration};
   animation: ${fadeIn} 2s ease-in;
+  &:hover {
+    color: pink;
+  }
 `;
 
 const BigTitle = styled(Title)`
@@ -38,7 +41,13 @@ export default function App() {
   return (
     <>
       <BlueBorderWrapper $shadow>
-        <Title $color="#0000ff" $decoration="underline" as="p">
+        <Title
+          $color="#0000ff"
+          $decoration="underline"
+          as="p"
+          title="...hello..."
+          onClickCapture={() => alert("hello!")}
+        >
           Hello, ReactJS!
         </Title>
         <BigTitle $color="#0000ff" $decoration="underline">
