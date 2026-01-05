ca62a24cbf363259ee22360673bbee2c1c9a2adc
kim0hun <dudgns9194@gmail.com>
2026-01-05 23:20:22 +0900

    105. tailwind css - 디렉티브

--- study/src/App.tsx
+++ study/src/App.tsx
@@ -3,9 +3,7 @@ export default function App() {
     <>
       <div className="flex flex-col items-center justify-center space-y-4">
         <button className="button">Click Me</button>
-        <button className="bg-green-500 text-white py-3 px-5 rounded border-none cursor-pointer transition-colors duration-300 hover:bg-green-600">
-          Click Me
-        </button>
+        <button className="btns">Click Me</button>
       </div>
     </>
   );
